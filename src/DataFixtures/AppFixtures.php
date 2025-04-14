da968973b936caf3b8026cee366c84589aaa4005
alex <alex.hmn18@gmail.com>
2025-04-14 14:57:31 +0200

    fixtures: ajout données de test pour les élèves

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,30 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Eleve;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    const ELEVES = [
+        ['Bob', 'Test'],
+        ['Jean', 'Jean'],
+        ['Marie', 'Poppins'],
+        ['Connor', 'Bedard']
+    ];
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        foreach (self::ELEVES as $eleveData) {
+            $eleve = new Eleve();
+            $eleve->setPrenom($eleveData[0])
+                ->setNom($eleveData[1]);
+
+            $manager->persist($eleve);
+        };
 
         $manager->flush();
     }
 }
+
