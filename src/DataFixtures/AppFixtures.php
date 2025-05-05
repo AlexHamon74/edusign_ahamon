c9c6c992b7213f1f1ee0ec6dc0c9b7e2f40fecc2
alex <alex.hmn18@gmail.com>
2025-05-05 10:17:02 +0200

    feat: création d'une entité User

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,27 +2,29 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Eleve;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    const ELEVES = [
-        ['Bob', 'Test'],
-        ['Jean', 'Jean'],
-        ['Marie', 'Poppins'],
-        ['Connor', 'Bedard']
+    const USERS = [
+        ['bob@test.com', 'bob123', 'Test', 'Bob'],
+        ['jean@jean.com', 'jean123', 'Jean', 'Jean'],
+        ['marie@poppins.com', 'marie123', 'Poppins', 'Marie'],
+        ['connor@bedard.com', 'connor123', 'Bedard', 'Connor']
     ];
 
     public function load(ObjectManager $manager): void
     {
-        foreach (self::ELEVES as $eleveData) {
-            $eleve = new Eleve();
-            $eleve->setPrenom($eleveData[0])
-                ->setNom($eleveData[1]);
+        foreach (self::USERS as $userData) {
+            $user = new User();
+            $user->setEmail($userData[0])
+                ->setPassword($userData[1])
+                ->setName($userData[2])
+                ->setFirstname($userData[3]);
 
-            $manager->persist($eleve);
+            $manager->persist($user);
         };
 
         $manager->flush();
